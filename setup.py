06b207b936c40d5effe0a04b7299ea5167159c89
s-t-e-a-l-t-h <mikecokina@gmail.com>
2018-10-04 16:55:46 +0200

    reanemd package name in setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='pytsdb',
+    name='pyopentsdb',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
@@ -30,7 +30,7 @@ setup(
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/s-t-e-a-l-t-h/pytsdb',
+    url='https://github.com/s-t-e-a-l-t-h/pyopentsdb',
 
     # Author details
     author='s-t-e-a-l-t-h',
